810794bac66c5d15008a765d7719ee40a5e7e301
Tom Medema <tommedema@gmail.com>
2017-09-26 21:05:17 +0800

    removed redundant test file

--- test.js
+++
@@ -1,5 +0,0 @@
-const iReplace = require('./src')
-const haystack = '<HEAD></HEAD>'
-const needle = '</head'
-const replacement = '<style>body { margin: 0}</style>$1'
-console.log(iReplace(haystack, needle, replacement))
